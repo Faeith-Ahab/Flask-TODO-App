46e12fd770f43d4605c9698f626d29711738ff22
Faeith-Ahab <faeithahab@gmail.com>
2024-03-13 17:03:24 +0300

    second commit

--- app.py
+++ app.py
@@ -1,40 +1,25 @@
-# Import the library, enable template, add request object(at the first addition of a request, add an accessor)
-# the code is appended according to the functions required to run.
+from flask import Flask, render_template, request,redirect,url_for
 
-from flask import Flask, render_template, request, redirect, url_for #
+app = Flask(__name__)
 
-# Create the app
-
-app = Flask(__name__, template_folder="templates")
-
-tasks=[]
-
-# Create the instance
+tasks = []
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html', tasks=tasks)
 
-
-# Create a decorator[@app] new task [post method]
-# Function must be on the same indentation as the decorator
-
-
-# Methods on the http protocol, create a variable that is going to store our task, we need a request object which has various methods we can pass through
-          
-            # action     # method
 @app.route('/add_task', methods=['POST'])
-def create_task():
-    task = request.form.get('task')      # value must be descriptive depending on what the field is, ie. task
-    tasks.append(task)                   # Add item to list by appending
-    return redirect(url_for("index"))    # redirect always has a particular path or URL
-                                         # It exists in Flask, there it is imported.
-                                         # URL is also a function
-    
+def add_task():
+    task = request.form.get('task')
+    tasks.append(task)
+    return render_template('index.html', tasks=tasks)
 
-# Enable debug mode
+@app.route('/delete/<int:index>')
+
+def delete_task(index):
+     if 0 <=index < len(tasks):
+         del tasks[index]
+     return redirect (url_for('home'))
 
 if __name__ == '__main__':
-   app.run(debug=True)
-   
-   
+    app.run(debug=True)
